4317dc01bfa7b94027b13cb19a6019d469ecb92a
Tejas-Chakkarwar <tejaschakkarwar@gmail.com>
2026-03-22 22:37:20 -0700

    rem

--- main.go
+++ main.go
@@ -4,5 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello from VS Code 🚀")
+
 	random()
+
+	fmt.Println(add(42, 43))
+
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+
+	fmt.Println(split(17))
 }
